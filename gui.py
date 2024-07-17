9253b499e5179bf8c088eceaae00838960cb5188
Jay Ravalani <jay.ravalani746@gmail.com>
2024-07-17 23:17:08 +0530

    Implement Show and Edit feature

--- gui.py
+++ gui.py
@@ -4,19 +4,39 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='existing_todo',
+                      enable_events=True,
+                      size=[45,10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("To-do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 14))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['existing_todo'][0])
     match event:
         case "Add":
             todos = functions.get_todos()
             todos.append(values['todo'] + "\n")
             functions.write_todos(todos)
+            window['existing_todo'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['existing_todo'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['existing_todo'].update(values=todos)
+
+        case "existing_todo":
+            window['todo'].update(value=values['existing_todo'][0])
+
         case sg.WINDOW_CLOSED:
             break
 
