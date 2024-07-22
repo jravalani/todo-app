d8932e53fd7c5b481879cd1c8fe8f18459326eb2
Jay Ravalani <jay.ravalani746@gmail.com>
2024-07-22 19:05:09 +0530

    Add static web page

---
+++ web.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Todo",placeholder="Enter a todo ",
+              label_visibility="hidden")
